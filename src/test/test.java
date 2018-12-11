e99752d42e6762c405c6166c4e54ca75cf183218
ming <kmj1993k@gmail.com>
2018-12-11 16:15:02 +0800

    加中文

--- src/test/test.java
+++ src/test/test.java
@@ -5,6 +5,7 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hey, it's our group project!!!");
+		System.out.println("這邊是中文");
 	}
 
 }
