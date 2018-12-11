4e90887d3ea2d51a33bc952037cc3031c8864d96
lichi <lichi@10.1.1.12>
2018-12-11 17:48:40 +0800

    test3

--- src/test/test.java
+++ src/test/test.java
@@ -6,7 +6,7 @@ public class test {
 		// TODO Auto-generated method stub
 		System.out.println("hey, it's our group project!!!");
 		System.out.println("這邊是中文");
-		System.out.println("test2");
+		System.out.println("test3");
 	}
 
 }
