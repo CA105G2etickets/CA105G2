690fc393eeaea21db6ac1f38e2139c8a357c39bc
Ming <Ming@Surface.mshome.net>
2018-12-26 02:00:38 +0800

    NEW transaction_history.model

---
+++ src/com/transaction_history/model/TransactionHistoryDAO_interface.java
@@ -0,0 +1,13 @@
+package com.transaction_history.model;
+
+import java.util.List;
+
+public interface TransactionHistoryDAO_interface {
+	
+	public void insert(TransactionHistoryVO transactionHistory);
+    public void update(TransactionHistoryVO transactionHistory);
+    public void delete(String transactionHistoryNo);
+    public TransactionHistoryVO findByPrimaryKey(String transactionHistoryNo);
+    public List<TransactionHistoryVO> getAll();
+    
+}
