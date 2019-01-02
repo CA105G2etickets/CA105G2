bfbc4d1c3e35d96c0267b7a7b85f3c8815dfc234
ming <kmj1993k@gmail.com>
2019-01-02 12:01:19 +0800

    administratorMVC

---
+++ src/com/administrator/controller/AdministratorServlet.java
@@ -0,0 +1,330 @@
+package com.administrator.controller;
+
+import java.io.*;
+import java.sql.Timestamp;
+import java.util.*;
+
+import javax.servlet.*;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.http.*;
+
+import com.administrator.model.*;
+
+
+@MultipartConfig(fileSizeThreshold = 1024 * 1024, maxFileSize = 5 * 1024 * 1024, maxRequestSize = 5 * 5 * 1024 * 1024)
+public class AdministratorServlet extends HttpServlet {
+
+//	public void doGet(HttpServletRequest req, HttpServletResponse res)
+//			throws ServletException, IOException {
+//		doPost(req, res);
+//	}
+
+	public void doPost(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
+
+		req.setCharacterEncoding("UTF-8");
+		String action = req.getParameter("action");
+		
+		
+		if ("getOne_For_Display".equals(action)) { // 來自select_page.jsp的請求
+
+			List<String> errorMsgs = new LinkedList<String>();
+			// Store this set in the request scope, in case we need to
+			// send the ErrorPage view.
+			req.setAttribute("errorMsgs", errorMsgs);
+
+			try {
+				/***************************1.接收請求參數 - 輸入格式的錯誤處理**********************/
+				String str = req.getParameter("administrator_no");
+				if (str == null || (str.trim()).length() == 0) {
+					errorMsgs.add("請輸入管理員編號");
+				}
+				// Send the use back to the form, if there were errors
+				if (!errorMsgs.isEmpty()) {
+					RequestDispatcher failureView = req
+							.getRequestDispatcher("/administrator/select_page.jsp");
+					failureView.forward(req, res);
+					return;//程式中斷
+				}
+				
+				String administrator_no = null;
+				try {
+					administrator_no = new String(str);
+				} catch (Exception e) {
+					errorMsgs.add("管理員編號格式不正確");
+				}
+				// Send the use back to the form, if there were errors
+				if (!errorMsgs.isEmpty()) {
+					RequestDispatcher failureView = req
+							.getRequestDispatcher("/administrator/select_page.jsp");
+					failureView.forward(req, res);
+					return;//程式中斷
+				}
+				
+				/***************************2.開始查詢資料*****************************************/
+				AdministratorService administratorService = new AdministratorService();
+				AdministratorVO administratorVO = administratorService.getOneAdministrator(administrator_no);
+				if (administratorVO == null) {
+					errorMsgs.add("查無資料");
+				}
+				// Send the use back to the form, if there were errors
+				if (!errorMsgs.isEmpty()) {
+					RequestDispatcher failureView = req
+							.getRequestDispatcher("/administrator/select_page.jsp");
+					failureView.forward(req, res);
+					return;
+				}
+				
+				/***************************3.查詢完成,準備轉交(Send the Success view)*************/
+				req.setAttribute("administratorVO", administratorVO); // 資料庫取出的empVO物件,存入req
+				String url = "/administrator/listOneAdministrator.jsp";
+				RequestDispatcher successView = req.getRequestDispatcher(url); // 成功轉交 listOneEmp.jsp
+				successView.forward(req, res);
+
+				/***************************其他可能的錯誤處理*************************************/
+			} catch (Exception e) {
+				errorMsgs.add("無法取得資料:" + e.getMessage());
+				RequestDispatcher failureView = req
+						.getRequestDispatcher("/administrator/select_page.jsp");
+				failureView.forward(req, res);
+			}
+		}
+		
+		
+		if ("getOne_For_Update".equals(action)) { // 來自listAllEmp.jsp的請求
+
+			List<String> errorMsgs = new LinkedList<String>();
+			// Store this set in the request scope, in case we need to
+			// send the ErrorPage view.
+			req.setAttribute("errorMsgs", errorMsgs);
+			
+			try {
+				/***************************1.接收請求參數****************************************/
+				String administrator_no = new String(req.getParameter("administrator_no"));
+				
+				/***************************2.開始查詢資料****************************************/
+				AdministratorService administratorService = new AdministratorService();
+				AdministratorVO administratorVO = administratorService.getOneAdministrator(administrator_no);
+				
+				/***************************3.查詢完成,準備轉交(Send the Success view)************/
+				req.setAttribute("administratorVO", administratorVO);         // 資料庫取出的empVO物件,存入req
+				String url = "/administrator/update_administrator_input.jsp";
+				RequestDispatcher successView = req.getRequestDispatcher(url);// 成功轉交 update_emp_input.jsp
+				successView.forward(req, res);
+
+				/***************************其他可能的錯誤處理**********************************/
+			} catch (Exception e) {
+				errorMsgs.add("無法取得要修改的資料:" + e.getMessage());
+				RequestDispatcher failureView = req
+						.getRequestDispatcher("/administrator/listAllAdministrator.jsp");
+				failureView.forward(req, res);
+			}
+		}
+		
+		
+		if ("update".equals(action)) { // 來自update_emp_input.jsp的請求
+			
+			List<String> errorMsgs = new LinkedList<String>();
+			// Store this set in the request scope, in case we need to
+			// send the ErrorPage view.
+			req.setAttribute("errorMsgs", errorMsgs);
+		
+			try {
+				/***************************1.接收請求參數 - 輸入格式的錯誤處理**********************/
+				String administrator_no = new String(req.getParameter("administrator_no").trim());
+				
+				String administrator_name = req.getParameter("administrator_name");
+				if (administrator_name == null || administrator_name.trim().length() == 0) {
+					errorMsgs.add("姓名請勿空白");
+				}
+				
+				String administrator_account = req.getParameter("administrator_account").trim();
+				if (administrator_account == null || administrator_account.trim().length() == 0) {
+					errorMsgs.add("帳號請勿空白");
+				}	
+				
+				String administrator_password = req.getParameter("administrator_password").trim();
+				if (administrator_password == null || administrator_password.trim().length() == 0) {
+					errorMsgs.add("密碼請勿空白");
+				}	
+				
+				byte[] administrator_picture = null;
+				Part part = req.getPart("administrator_picture");
+				try {
+					String uploadFileName = part.getSubmittedFileName();
+					if (uploadFileName != null && part.getContentType() != null) {
+						InputStream in = part.getInputStream();
+						administrator_picture = new byte[in.available()];
+						in.read(administrator_picture);
+						in.close();
+					}
+				} catch (FileNotFoundException e) {
+					errorMsgs.add("找不到檔案");
+				}
+				if (part.getSize() == 0) {
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+				}
+				
+				String administrator_status = req.getParameter("administrator_status").trim();
+				if (administrator_status == null || administrator_status.trim().length() == 0) {
+					errorMsgs.add("狀態請勿空白");
+				}
+				
+				AdministratorVO administratorVO = new AdministratorVO();
+				administratorVO.setAdministrator_no(administrator_no);
+				administratorVO.setAdministrator_name(administrator_name);
+				administratorVO.setAdministrator_account(administrator_account);
+				administratorVO.setAdministrator_password(administrator_password);
+//				administratorVO.setCreation_date(creation_date);
+				administratorVO.setAdministrator_picture(administrator_picture);
+				administratorVO.setAdministrator_status(administrator_status);
+				
+
+				// Send the use back to the form, if there were errors
+				if (!errorMsgs.isEmpty()) {
+					req.setAttribute("administratorVO", administratorVO); // 含有輸入格式錯誤的empVO物件,也存入req
+					RequestDispatcher failureView = req
+							.getRequestDispatcher("/administrator/update_administrator_input.jsp");
+					failureView.forward(req, res);
+					return; //程式中斷
+				}
+				
+				/***************************2.開始修改資料*****************************************/
+				AdministratorService administratorService = new AdministratorService();
+				administratorService.updateAdministrator(administrator_no, administrator_name, administrator_account, administrator_password, administrator_picture, administrator_status);
+				
+				/***************************3.修改完成,準備轉交(Send the Success view)*************/
+				req.setAttribute("administratorVO", administratorVO); // 資料庫update成功後,正確的的empVO物件,存入req
+				String url = "/administrator/listOneAdministrator.jsp";
+				RequestDispatcher successView = req.getRequestDispatcher(url); // 修改成功後,轉交listOneEmp.jsp
+				successView.forward(req, res);
+
+				/***************************其他可能的錯誤處理*************************************/
+			} catch (Exception e) {
+				errorMsgs.add("修改資料失敗:"+e.getMessage());
+				RequestDispatcher failureView = req
+						.getRequestDispatcher("/administrator/update_administrator_input.jsp");
+				failureView.forward(req, res);
+			}
+		}
+
+        if ("insert".equals(action)) { // 來自addEmp.jsp的請求  
+			
+			List<String> errorMsgs = new LinkedList<String>();
+			// Store this set in the request scope, in case we need to
+			// send the ErrorPage view.
+			req.setAttribute("errorMsgs", errorMsgs);
+
+			try {
+				/***********************1.接收請求參數 - 輸入格式的錯誤處理*************************/
+				String administrator_name = req.getParameter("administrator_name");
+				if (administrator_name == null || administrator_name.trim().length() == 0) {
+					errorMsgs.add("姓名請勿空白");
+				}
+				
+				String administrator_account = req.getParameter("administrator_account").trim();
+				if (administrator_account == null || administrator_account.trim().length() == 0) {
+					errorMsgs.add("帳號請勿空白");
+				}	
+				
+				String administrator_password = req.getParameter("administrator_password").trim();
+				if (administrator_password == null || administrator_password.trim().length() == 0) {
+					errorMsgs.add("密碼請勿空白");
+				}	
+				
+				byte[] administrator_picture = null;
+				Part part = req.getPart("administrator_picture");
+				try {
+					String uploadFileName = part.getSubmittedFileName();
+					if (uploadFileName != null && part.getContentType() != null) {
+						InputStream in = part.getInputStream();
+						administrator_picture = new byte[in.available()];
+						in.read(administrator_picture);
+						in.close();
+					}
+				} catch (FileNotFoundException e) {
+					errorMsgs.add("找不到檔案");
+				}
+				if (part.getSize() == 0) {
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////
+				}
+				
+				String administrator_status = req.getParameter("administrator_status").trim();
+				if (administrator_status == null || administrator_status.trim().length() == 0) {
+					errorMsgs.add("狀態請勿空白");
+				}
+				
+				AdministratorVO administratorVO = new AdministratorVO();
+				administratorVO.setAdministrator_name(administrator_name);
+				administratorVO.setAdministrator_account(administrator_account);
+				administratorVO.setAdministrator_password(administrator_password);
+//				administratorVO.setCreation_date(creation_date);
+				administratorVO.setAdministrator_picture(administrator_picture);
+				administratorVO.setAdministrator_status(administrator_status);
+
+				// Send the use back to the form, if there were errors
+				if (!errorMsgs.isEmpty()) {
+					req.setAttribute("administratorVO", administratorVO); // 含有輸入格式錯誤的empVO物件,也存入req
+					RequestDispatcher failureView = req
+							.getRequestDispatcher("/administrator/addAdministrator.jsp");
+					failureView.forward(req, res);
+					return;
+				}
+				
+				/***************************2.開始新增資料***************************************/
+				AdministratorService administratorService = new AdministratorService();
+				administratorVO = administratorService.addAdministrator(administrator_name, administrator_account, administrator_password, administrator_picture, administrator_status);				
+				
+				/***************************3.新增完成,準備轉交(Send the Success view)***********/
+				String url = "/administrator/listAllAdministrator.jsp";
+				RequestDispatcher successView = req.getRequestDispatcher(url); // 新增成功後轉交listAllEmp.jsp
+				successView.forward(req, res);				
+				
+				/***************************其他可能的錯誤處理**********************************/
+			} catch (Exception e) {
+				errorMsgs.add(e.getMessage());
+				RequestDispatcher failureView = req
+						.getRequestDispatcher("/administrator/addAdministrator.jsp");
+				failureView.forward(req, res);
+			}
+		}
+		
+		
+		if ("delete".equals(action)) { // 來自listAllEmp.jsp
+
+			List<String> errorMsgs = new LinkedList<String>();
+			// Store this set in the request scope, in case we need to
+			// send the ErrorPage view.
+			req.setAttribute("errorMsgs", errorMsgs);
+	
+			try {
+				/***************************1.接收請求參數***************************************/
+				String administrator_no = new String(req.getParameter("administrator_no"));
+				
+				/***************************2.開始刪除資料***************************************/
+				AdministratorService administratorService = new AdministratorService();
+				administratorService.deleteAdministrator(administrator_no);
+				
+				/***************************3.刪除完成,準備轉交(Send the Success view)***********/								
+				String url = "/administrator/listAllAdministrator.jsp";
+				RequestDispatcher successView = req.getRequestDispatcher(url);// 刪除成功後,轉交回送出刪除的來源網頁
+				successView.forward(req, res);
+				
+				/***************************其他可能的錯誤處理**********************************/
+			} catch (Exception e) {
+				errorMsgs.add("刪除資料失敗:"+e.getMessage());
+				RequestDispatcher failureView = req
+						.getRequestDispatcher("/administrator/listAllAdministrator.jsp");
+				failureView.forward(req, res);
+			}
+		}
+	}
+}
--- src/com/administrator/model/AdministratorDAO.java
+++ src/com/administrator/model/AdministratorDAO.java
@@ -1,5 +1,270 @@
 package com.administrator.model;
 
-public class AdministratorDAO {
+import java.util.*;
+import java.sql.*;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class AdministratorDAO implements AdministratorDAO_interface {
+
+	// 一個應用程式中,針對一個資料庫 ,共用一個DataSource即可
+	private static DataSource ds = null;
+	static {
+		try {
+			Context ctx = new InitialContext();
+			ds = (DataSource) ctx.lookup("java:comp/env/jdbc/ETIckeTsDB");
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private static final String INSERT_STMT = 
+			"INSERT INTO ADMINISTRATOR (ADMINISTRATOR_NO,ADMINISTRATOR_NAME,ADMINISTRATOR_ACCOUNT,ADMINISTRATOR_PASSWORD,CREATION_DATE,ADMINISTRATOR_PICTURE,ADMINISTRATOR_STATUS) VALUES ('A'||LPAD(to_char(administrator_no_seq.NEXTVAL), 3, '0'), ?, ?, ?,CURRENT_TIMESTAMP, ?, ?)";
+	private static final String UPDATE = 
+			"UPDATE ADMINISTRATOR SET ADMINISTRATOR_NAME = ?, ADMINISTRATOR_ACCOUNT = ?, ADMINISTRATOR_PASSWORD = ?, ADMINISTRATOR_PICTURE = ?, ADMINISTRATOR_STATUS = ? WHERE ADMINISTRATOR_NO = ?";
+	private static final String DELETE = 
+			"DELETE FROM ADMINISTRATOR WHERE ADMINISTRATOR_NO = ?";
+	private static final String GET_ONE_STMT = 
+			"SELECT * FROM ADMINISTRATOR WHERE ADMINISTRATOR_NO = ?";
+	private static final String GET_ALL_STMT = 
+			"SELECT * FROM ADMINISTRATOR ORDER BY ADMINISTRATOR_NO";
+
+	@Override
+	public void insert(AdministratorVO administratorVO) {
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		try {
+
+			con = ds.getConnection();
+			pstmt = con.prepareStatement(INSERT_STMT);
+
+			pstmt.setString(1, administratorVO.getAdministrator_name());
+			pstmt.setString(2, administratorVO.getAdministrator_account());
+			pstmt.setString(3, administratorVO.getAdministrator_password());
+//			pstmt.setTimestamp(5, administratorVO.getCreation_date());
+			pstmt.setBytes(5, administratorVO.getAdministrator_picture());
+			pstmt.setString(6, administratorVO.getAdministrator_status());
+
+			pstmt.executeUpdate();
+
+		} catch (SQLException se) {
+			throw new RuntimeException("BuBu!"
+					+ se.getMessage());
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+
+	}
+
+	@Override
+	public void update(AdministratorVO administratorVO) {
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		try {
+
+			con = ds.getConnection();
+			pstmt = con.prepareStatement(UPDATE);
+
+			pstmt.setString(1, administratorVO.getAdministrator_name());
+			pstmt.setString(2, administratorVO.getAdministrator_account());
+			pstmt.setString(3, administratorVO.getAdministrator_password());
+//			pstmt.setTimestamp(4, administratorVO.getCreation_date());
+			pstmt.setBytes(4, administratorVO.getAdministrator_picture());
+			pstmt.setString(5, administratorVO.getAdministrator_status());
+			pstmt.setString(6, administratorVO.getAdministrator_no());
+
+			pstmt.executeUpdate();
+
+		} catch (SQLException se) {
+			throw new RuntimeException("BuBu!"
+					+ se.getMessage());
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+
+	}
+
+	@Override
+	public void delete(String administrator_no) {
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+
+		try {
+
+			con = ds.getConnection();
+			pstmt = con.prepareStatement(DELETE);
+
+			pstmt.setString(1, administrator_no);
+
+			pstmt.executeUpdate();
+
+		} catch (SQLException se) {
+			throw new RuntimeException("BuBu!"
+					+ se.getMessage());
+		} finally {
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+
+	}
+
+	@Override
+	public AdministratorVO findByPrimaryKey(String administrator_no) {
+
+		AdministratorVO administratorVO = null;
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+
+			con = ds.getConnection();
+			pstmt = con.prepareStatement(GET_ONE_STMT);
+
+			pstmt.setString(1, administrator_no);
+
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				administratorVO = new AdministratorVO();
+				administratorVO.setAdministrator_no(rs.getString("ADMINISTRATOR_NO"));
+				administratorVO.setAdministrator_name(rs.getString("ADMINISTRATOR_NAME"));
+				administratorVO.setAdministrator_account(rs.getString("ADMINISTRATOR_ACCOUNT"));
+				administratorVO.setAdministrator_password(rs.getString("ADMINISTRATOR_PASSWORD"));
+				administratorVO.setCreation_date(rs.getTimestamp("CREATION_DATE"));
+				administratorVO.setAdministrator_picture(rs.getBytes("ADMINISTRATOR_PICTURE"));
+				administratorVO.setAdministrator_status(rs.getString("ADMINISTRATOR_STATUS"));
+				
+			}
+
+		} catch (SQLException se) {
+			throw new RuntimeException("BuBu!"
+					+ se.getMessage());
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+		return administratorVO;
+	}
+
+	@Override
+	public List<AdministratorVO> getAll() {
+		List<AdministratorVO> list = new ArrayList<AdministratorVO>();
+		AdministratorVO administratorVO = null;
+
+		Connection con = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+
+			con = ds.getConnection();
+			pstmt = con.prepareStatement(GET_ALL_STMT);
+			rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				administratorVO = new AdministratorVO();
+				administratorVO.setAdministrator_no(rs.getString("ADMINISTRATOR_NO"));
+				administratorVO.setAdministrator_name(rs.getString("ADMINISTRATOR_NAME"));
+				administratorVO.setAdministrator_account(rs.getString("ADMINISTRATOR_ACCOUNT"));
+				administratorVO.setAdministrator_password(rs.getString("ADMINISTRATOR_PASSWORD"));
+				administratorVO.setCreation_date(rs.getTimestamp("CREATION_DATE"));
+				administratorVO.setAdministrator_picture(rs.getBytes("ADMINISTRATOR_PICTURE"));
+				administratorVO.setAdministrator_status(rs.getString("ADMINISTRATOR_STATUS"));
+				list.add(administratorVO);
+			}
+
+		} catch (SQLException se) {
+			throw new RuntimeException("BuBu!"
+					+ se.getMessage());
+		} finally {
+			if (rs != null) {
+				try {
+					rs.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException se) {
+					se.printStackTrace(System.err);
+				}
+			}
+			if (con != null) {
+				try {
+					con.close();
+				} catch (Exception e) {
+					e.printStackTrace(System.err);
+				}
+			}
+		}
+		return list;
+	}
 }
