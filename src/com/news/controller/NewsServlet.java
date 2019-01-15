9da793bfb7d081419197c1016167a26c70d005c4
ming <kmj1993k@gmail.com>
2019-01-16 03:24:16 +0800

    首頁~

--- src/com/news/controller/NewsServlet.java
+++ src/com/news/controller/NewsServlet.java
@@ -13,10 +13,10 @@ import com.news.model.NewsVO;
 
 public class NewsServlet extends HttpServlet {
 
-//	public void doGet(HttpServletRequest req, HttpServletResponse res)
-//			throws ServletException, IOException {
-//		doPost(req, res);
-//	}
+	public void doGet(HttpServletRequest req, HttpServletResponse res)
+			throws ServletException, IOException {
+		doPost(req, res);
+	}
 
 	public void doPost(HttpServletRequest req, HttpServletResponse res)
 			throws ServletException, IOException {
@@ -85,6 +85,70 @@ public class NewsServlet extends HttpServlet {
 				errorMsgs.add("無法取得資料:" + e.getMessage());
 				RequestDispatcher failureView = req
 						.getRequestDispatcher("/backend/news/select_page.jsp");
+				failureView.forward(req, res);
+			}
+		}
+		
+		if ("getOne_For_Display_front".equals(action)) { // 來自select_page.jsp的請求
+			
+			List<String> errorMsgs = new LinkedList<String>();
+			// Store this set in the request scope, in case we need to
+			// send the ErrorPage view.
+			req.setAttribute("errorMsgs", errorMsgs);
+			
+			try {
+				/***************************1.接收請求參數 - 輸入格式的錯誤處理**********************/
+				String str = req.getParameter("news_no");
+				if (str == null || (str.trim()).length() == 0) {
+					errorMsgs.add("請輸入公告編號");
+				}
+				// Send the use back to the form, if there were errors
+				if (!errorMsgs.isEmpty()) {
+					RequestDispatcher failureView = req
+							.getRequestDispatcher("/frontend/index.jsp");
+					failureView.forward(req, res);
+					return;//程式中斷
+				}
+				
+				String news_no = null;
+				try {
+					news_no = new String(str);
+				} catch (Exception e) {
+					errorMsgs.add("公告編號格式不正確");
+				}
+				// Send the use back to the form, if there were errors
+				if (!errorMsgs.isEmpty()) {
+					RequestDispatcher failureView = req
+							.getRequestDispatcher("/frontend/index.jsp");
+					failureView.forward(req, res);
+					return;//程式中斷
+				}
+				
+				/***************************2.開始查詢資料*****************************************/
+				NewsService newsService = new NewsService();
+				NewsVO newsVO = newsService.getOneNews(news_no);
+				if (newsVO == null) {
+					errorMsgs.add("查無資料");
+				}
+				// Send the use back to the form, if there were errors
+				if (!errorMsgs.isEmpty()) {
+					RequestDispatcher failureView = req
+							.getRequestDispatcher("/frontend/index.jsp");
+					failureView.forward(req, res);
+					return;
+				}
+				
+				/***************************3.查詢完成,準備轉交(Send the Success view)*************/
+				req.setAttribute("newsVO", newsVO); // 資料庫取出的empVO物件,存入req
+				String url = "/frontend/news/oneNews.jsp";
+				RequestDispatcher successView = req.getRequestDispatcher(url); // 成功轉交 listOneEmp.jsp
+				successView.forward(req, res);
+				
+				/***************************其他可能的錯誤處理*************************************/
+			} catch (Exception e) {
+				errorMsgs.add("無法取得資料:" + e.getMessage());
+				RequestDispatcher failureView = req
+						.getRequestDispatcher("/frontend/index.jsp");
 				failureView.forward(req, res);
 			}
 		}
